bab048287d36b1e696fa085b35336dd1a374d6f7
shanakaChathu <shanakado@gmail.com>
2021-10-03 09:28:27 +0530

    pipeline executed

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import numpy as np
+import yaml
+import joblib 
+
+webapp_root = "webapp"
+params_path = "params.yaml"
+
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
+
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data).tolist()[0]
+    return prediction 
+
+def form_response(dict_request):
+    data = dict_request.values()
+    data = [list(map(float, data))]
+    response = predict(data)
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                dict_req = dict(request.form)
+                response = form_response(dict_req)
+                return render_template("index.html", response=response)
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+
+            return render_template("404.html", error=error)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
